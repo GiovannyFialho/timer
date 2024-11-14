35b68fa7e5b07411438fd555253d7894afe3ddb0
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2024-11-14 11:07:11 -0300

    criacao das paginas Home e Historico

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { ThemeProvider } from "styled-components";
 
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 import { GlobalStyles } from "./styles/global";
 import { defaultTheme } from "./styles/theme/default";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
 
-      <h1>opa</h1>
+      <GlobalStyles />
     </ThemeProvider>
   );
 }
